c64a38c0b0fdaae3708832a139a8b8187895b7c5
JohnnieDom <johnniedom1@gmail.com>
2023-01-01 14:07:08 +0100

    Happy New Year

--- client/script.js
+++ client/script.js
@@ -1,83 +1,106 @@
-import bot from  './assets/bot.svg';
-import user from './assets/user.svg';
+import bot from "./assets/bot.svg";
+import user from "./assets/user.svg";
 
 const form = document.querySelector(`form`);
-const cheatContainer  = document.querySelector(`#chat_container`);
+const cheatContainer = document.querySelector(`#chat_container`);
 
+//IMPLEMENTING LOADING ANIMATION
 let loadInterval;
-function loader(element){
-    element.textContent=``;
-    loadInterval = setInterval(() => {
-        element.textContent += `.`;
-        if ( element.textContent === `....`){
-            element.textContent=``;
-        }
-    }, 300);
+function loader(element) {
+  element.textContent = ``;
+  loadInterval = setInterval(() => {
+    element.textContent += `.`;
+    if (element.textContent === `....`) {
+      element.textContent = ``;
+    }
+  }, 300);
 }
 
+// implementing typing like a Human
+function typeText(element, text) {
+  let index = 0;
 
-// implementing thinking e.g typing like a Human 
-function typeText (element, text){
-    let index = 0;
- 
-    let interval = setInterval(() => {
-        if(element < text.length){
-            element.innerHTML += text.chartAt(index);
-            index++;
-        }else{
-            clearInterval(interval)
-        }
-    }, 20);
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20);
 }
 
- // Generating A unique id for a User and ai
-function generateUniqueId(){
-    const timestamp = Date.now()
-    const randomNumber = Math.random()
-    const hexadecimalString = randomNumber.toString(16)
-    return `id ${timestamp}-${hexadecimalString}`
+// Generating A unique id for a User and ai
+function generateUniqueId() {
+  const timestamp = Date.now();
+  console.log(timestamp);
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+  return `id-${timestamp}-${hexadecimalString}`;
 }
 console.log(generateUniqueId());
 // implementing Chat strip
-function chatStrip(isAl, value, uniqueId){
-    return(
-        `
+function chatStrip(isAl, value, uniqueId) {
+  return `
         <div class="wrapper ${isAl && `ai`}">
         <div class= "chat">
         <div class= "profile">
-        <img src="${isAl ? bot: user}" alt="${isAl ? 'bot': 'user'}">
+        <img src="${isAl ? bot : user}" alt="${isAl ? "bot" : "user"}">
         </div>
         <div class= "message" id="${uniqueId}">${value}</div>
         </div>
         </div>
-        `
-    )
+        `;
 }
 
-const handleSubmit = async (e)=>{
-    e.preventDefault();
+const handleSubmit = async (e) => {
+  e.preventDefault();
 
-    const data = new FormData(form)
+  const data = new FormData(form);
 
-    // generate the user chat strip
-    cheatContainer.innerHTML += chatStrip(false, data.get(`prompt`) );
+  // generate the user chat strip
+  cheatContainer.innerHTML += chatStrip(false, data.get(`prompt`));
 
-    form.reset();
+  form.reset();
 
-    // bot chart strip
-    const uniqueId = generateUniqueId();
-    cheatContainer.innerHTML += chatStrip(true, " ", uniqueId);
+  // bot chart strip
+  const uniqueId = generateUniqueId();
+  cheatContainer.innerHTML += chatStrip(true, " ", uniqueId);
 
-    cheatContainer.scrollTop = cheatContainer.scrollHeight;
+  cheatContainer.scrollTop = cheatContainer.scrollHeight;
 
-    const messageDiv = document.getElementById(uniqueId)
+  const messageDiv = document.getElementById(uniqueId);
 
-    loader(messageDiv)
-}
+  loader(messageDiv);
 
-form.addEventListener(`submit`, handleSubmit)
-form.addEventListener('keyup', (e)=> {
-    if (e.keycode === 13){
-        handleSubmit(e)
-    }
-}) 
+  // fetch data from server  -> bot's response
+  const response = await fetch(`http://localhost:5000`, {
+      method: `POST`,
+      headers:{
+        "Content-Type": "application/json"
+      }, 
+      body: JSON.stringify({
+        prompt: data.get(`prompt`)
+      })
+  })
+  clearInterval(loadInterval)
+  messageDiv.innerHTML = ``;
+  if(response.ok){
+    const data =  await response.json();
+    // console.log(await response.json());
+    const parsedData = data.bot.trim();
+
+    typeText(messageDiv, parsedData)
+  }else{
+    const err = await response.text();
+    messageDiv.innerHTML = `Something went Wrong`;
+    alert(err)
+  }
+};
+
+form.addEventListener(`submit`, handleSubmit);
+form.addEventListener("keyup", (e) => {
+  if (e.keycode === 13) {
+    handleSubmit(e);
+  }
+});
---
+++ server/server.js
@@ -0,0 +1,50 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+console.log(process.env.OPENAI_API_KEY);
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+app.use(cors()); // it allows you make cross origin request (it allow us server to called from the front-end)
+app.use(express.json()); //  it allows you pass json from the front-end to the Backend
+
+// Dummy Root rout
+app.get(`/`, async (req, res) => {
+  res.status(200).send({
+    message: `Hello from codex`,
+  });
+});
+
+app.post(`/`, async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+
+    
+    // Getting a response form the openai
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0,
+      max_tokens:4000,
+      top_p: 1.0,
+      frequency_penalty: 0.5,
+      presence_penalty: 0.0,
+    });
+    res.status(200).send({
+        bot: response.data.choices[0].text
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({error})
+  }
+});
+
+app.listen(5000, ()=> console.log(`server is running on port http://localhost:5000`))
